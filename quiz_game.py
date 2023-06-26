7ad8f86cc28d8d2c7bcbbcc7258ce2c32364ba4a
Aiswariyat <taiswariya5@gmail.com>
2023-06-25 20:07:11 -0500

    First commit

---
+++ quiz_game.py
@@ -0,0 +1,23 @@
+print("Welcome to the Polygon Quiz")
+playing = input("Do you want to play the game? ")
+if playing.lower()!="yes":
+    quit()
+
+answer=input("how many sides does a triangle have(type in words)? ")
+if answer.lower()=="three":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer=input("how many sides does a rectangle have(type in words)? ")
+if answer.lower()=="four":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer=input("how many sides does a quadilateral have(type in words)? ")
+if answer.lower()=="five":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
