db725cef3a1155c7c504894cfc78fe32d3916327
Aiswariyat <taiswariya5@gmail.com>
2023-06-27 13:42:48 -0500

    second commit

--- number_guesser.py
+++ number_guesser.py
@@ -5,15 +5,14 @@ playing=input("Do you want to play the game?" )
 if playing.lower()!="yes":
     quit()
 
-randomNumber= random.randint(1,10)
+randomNumber= random.randint(0,10)
 
-for i in range(0,10):
+while True:
     number=int(input("type a number between 1 to 10: "))
-    print(number)
     if number==randomNumber:
         print("You guessed it correctly")
         break
         
     else:
         print("your guess is incorrect, try again")
-        i=i+1
+        
